ed32d68b43780cf90a2a2e8cde4c7a41e18e6156
Szymon <sszulinski@gmail.com>
2021-03-14 20:20:29 +0100

    task 18.2 - order price display

---
+++ src/components/features/OrderSummary/OrderSummary.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './OrderSummary.scss';
+import {calculateTotal} from '../../../utils/calculateTotal';
+import {formatPrice} from '../../../utils/formatPrice';
+//
+
+const OrderSummary = ({tripCost, options}) =>
+  (
+    <h2 className={styles.component}> 
+        Total: {formatPrice(calculateTotal(tripCost,options))} 
+    </h2>
+  );
+
+
+OrderSummary.propTypes = 
+{
+  tripCost: PropTypes.string,
+  options: PropTypes.object,
+};
+
+export default OrderSummary;
