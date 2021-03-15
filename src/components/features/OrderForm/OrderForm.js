3180587579493947aedf228e359df3cb23106a46
Szymon <sszulinski@gmail.com>
2021-03-15 23:43:50 +0100

    add OrderOptionDropdown, OOCheckboxes, OOIcons & OONumber

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import PropTypes, { func } from 'prop-types';
 import {Row, Col} from 'react-flexbox-grid';
 import OrderSummary from '../OrderSummary/OrderSummary';
+import pricing from '../../../data/pricing.json';
+import OrderOption from '../OrderOption/OrderOption';
 
-const OrderForm = ({tripCost, options}) => 
+const OrderForm = ({tripCost, options, setOrderOption}) => 
   (
     <Row>
+      {pricing.map(({...option}) => (
+        <Col md={4} key={option.id}>
+          <OrderOption setOrderOption={setOrderOption} currentValue={options[option.id]} {...option} />
+        </Col>
+      ))}
       <Col xs={12}> 
         <OrderSummary tripCost={tripCost} options={options} />
       </Col>
@@ -16,6 +23,7 @@ OrderForm.propTypes =
   {
     tripCost: PropTypes.string,
     options: PropTypes.object,
+    setOrderOption: func,
   };
 
 export default OrderForm;
