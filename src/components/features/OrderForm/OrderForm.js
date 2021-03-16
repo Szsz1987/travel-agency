fa6077f96694e7e9a47a358e99ad27dda45bbb07
Szymon <sszulinski@gmail.com>
2021-03-16 22:45:15 +0100

    fix bug

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -2,20 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {Row, Col} from 'react-flexbox-grid';
 import OrderSummary from '../OrderSummary/OrderSummary';
+import pricing from '../../../data/pricing.json';
+import OrderOption from '../OrderOption/OrderOption';
 
-const OrderForm = ({tripCost, options}) => 
-  (
-    <Row>
-      <Col xs={12}> 
-        <OrderSummary tripCost={tripCost} options={options} />
+const OrderForm = ({tripCost, options, setOrderOption}) => (
+  <Row>
+    {pricing.map(({...option}) => (
+      <Col md={4} key={option.id}>
+        <OrderOption setOrderOption={setOrderOption} currentValue={options[option.id]} {...option} />
       </Col>
-    </Row>
-  );
+    ))}
+    <Col xs={12}>
+      <OrderSummary tripCost={tripCost} options={options} />
+    </Col>
+  </Row>
+);
 
-OrderForm.propTypes = 
-  {
-    tripCost: PropTypes.string,
-    options: PropTypes.object,
-  };
+OrderForm.propTypes = {
+  tripCost: PropTypes.string,
+  options: PropTypes.object,
+  setOrderOption: PropTypes.func,
+};
 
 export default OrderForm;
