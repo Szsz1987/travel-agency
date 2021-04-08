ee62fd8cb47062909620285084ca39f52fd8377e
Szymon <sszulinski@gmail.com>
2021-04-08 22:05:31 +0200

    add DaysToSummer.test.js

---
+++ src/components/features/DaysToSummer/DaysToSummer.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import DaysToSummer from './DaysToSummer';
+
+const mockProps = {
+  firstDate: new Date(2020,1,1),
+  borderDate: new Date(2020,5,20),
+  secondDate: new Date(2020,8,25),
+};
+const select = {
+  title: '.title',
+};
+
+describe ('Component DaysToSummer', () => {
+  it ('should render without crashing', () => {
+    const component = shallow(<DaysToSummer/>);
+    expect (component).toBeTruthy();
+  });
+  it ('should render component with h3 inside', () => {
+    const component = shallow (<DaysToSummer/>);
+    expect (component.exists(select.title)).toEqual(true);
+  });
+});
+
+describe ('should give proper number of days left to summer on specific dates', () => {
+  it('01.01.20', () => {
+    const component = shallow (<DaysToSummer date={mockProps.firstDate}/>);
+    expect(component.find(select.title).text()).toEqual('171 days left to summer!');
+  });
+  it('20.06.2020', () => { 
+    const component = shallow (< DaysToSummer date={mockProps.borderDate}/>);
+    expect (component.find(select.title).text()).toEqual('1 day left to summer!');
+  });
+  it('25.09.2020', () => {
+    const component = shallow (<DaysToSummer date={mockProps.secondDate}/>);
+    expect (component.find(select.title).text()).toEqual('269 days left to summer!');
+  });
+});
